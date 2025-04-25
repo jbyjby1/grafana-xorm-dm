ff17e9022f182d3bc20945130e4aa893b0b51398
jinboyu <jbypaas@sina.com>
2025-04-25 09:43:43 +0800

    增加部分日志打印，测试是否使用本类新建engine

--- xorm.go
+++ xorm.go
@@ -60,6 +60,8 @@ func init() {
 // NewEngine new a db manager according to the parameter. Currently support four
 // drivers
 func NewEngine(driverName string, dataSourceName string) (*Engine, error) {
+	currentLog := log.New("xorm")
+
 	driver := core.QueryDriver(driverName)
 	if driver == nil {
 		return nil, fmt.Errorf("Unsupported driver name: %v", driverName)
@@ -69,8 +71,10 @@ func NewEngine(driverName string, dataSourceName string) (*Engine, error) {
 	if err != nil {
 		return nil, err
 	}
-
+	currentLog.Warn("[DB] Database uri: " + uri)
+	currentLog.Warn("[DB] Database type: " + uri.DbType)
 	dialect := core.QueryDialect(uri.DbType)
+	currentLog.Warn("[DB] Dialect: ", dialect)
 	if dialect == nil {
 		return nil, fmt.Errorf("Unsupported dialect type: %v", uri.DbType)
 	}
