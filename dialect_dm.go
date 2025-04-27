7cf32989eef43fbf8344b8368d9f26e80442d76e
jinboyu <jbypaas@sina.com>
2025-04-27 18:44:06 +0800

    达梦方言增加logger字段用于记录日志

--- dialect_dm.go
+++ dialect_dm.go
@@ -164,11 +164,11 @@ var (
 type dm struct {
 	core.Base
 	rowFormat string
-	currentLog log.Logger
+	logger log.Logger
 }
 
 func (db *dm) Init(d *core.DB, uri *core.Uri, drivername, dataSourceName string) error {
-	return db.Base.Init(d, db, uri, drivername, dataSourceName) 
+	return db.Base.Init(d, db, uri, drivername, dataSourceName)
 }
 
 func (db *dm) SetParams(params map[string]string) {
