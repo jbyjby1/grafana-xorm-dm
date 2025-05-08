7ce46c39e67e6fbd13998dfc8d6e077547bf7522
jinboyu <jbypaas@sina.com>
2025-05-08 11:42:34 +0800

    修复编译错误

--- session.go
+++ session.go
@@ -103,20 +103,14 @@ func (session *Session) Init() {
 
 	// 注册Before钩子
 	session.Before(func(bean interface{}) {
-		if stmt, ok := bean.(*xorm.Statement); ok {
+		if stmt, ok := bean.(*Statement); ok {
 			originalSQL := stmt.SQL.String()
-			preparedSQL := prepareSQL(originalSQL)
+			preparedSQL := strings.ReplaceAll(strings.ReplaceAll(originalSQL, "\n", " "), "\t", " ")
 			stmt.SQL.Reset() // 清空原有的SQL
 			stmt.SQL.WriteString(preparedSQL) // 设置新的SQL
 		}
 	})
 
-}
-
-// prepareSQL 在执行前对SQL进行预处理
-func (session *Session) prepareSQL(sql string) string {
-    // 将\n和\t替换为空格
-    return strings.ReplaceAll(strings.ReplaceAll(sql, "\n", " "), "\t", " ")
 }
 
 
